9d32269fdd3c58d84bbc997b68267504e9dbf9f0
Demur Rumed <gunkmute@gmail.com>
2015-10-18 16:09:36 +0000

    sform

--- vm.rs
+++ vm.rs
@@ -2,19 +2,18 @@ use std::vec::*;
 use std::char;
 use std::io::{Read,stdin};
 
+#[derive(Clone)]
 pub enum Obj{
 	S(String),
 	I(i64),
 }
-
 struct Word{
 	op: String,
 	code: String,
 }
-
 struct Vmem{
 	st : Vec<Obj>,
-	ws : Vec<Word>
+	ws : Vec<Word>,
 }
 
 fn add(vm : &mut Vmem){
@@ -52,7 +51,26 @@ fn pick(vm : &mut Vmem){
 }
 
 fn sform(vm : &mut Vmem){
-	// TODO	
+	let slen = vm.st.len();
+	if slen < 2 { return vm.st.clear() }
+	if let (Obj::I(popxi), Obj::I(basei)) = (vm.st[slen-1].clone(), vm.st[slen-2].clone()) {
+		if popxi < 0 || basei < 0 { return }
+		let (popx, base) = (popxi as usize, basei as usize);
+		if popx+2 > slen || base+2 > slen { return }
+		let (spopx, sbase) : (usize, usize) = (slen-popx-2, slen-base-2);
+		for i in 0 .. popx {
+			if let Obj::I(offs) = vm.st[spopx+i] {
+				if offs >= 0 {
+					let offu = offs as usize;
+					if offu <= spopx { vm.st[spopx + i] = vm.st[spopx - offu].clone() }
+				}
+			}
+		}
+		for i in 0 .. popx {
+			vm.st[sbase+i] = vm.st[spopx+i].clone();
+		}
+		vm.st.truncate(sbase+popx);
+	}
 }
 
 fn printobj(vm : &mut Vmem){
@@ -101,19 +119,23 @@ fn execword(op : &str, vm : &mut Vmem){
 		return vm.st.push(Obj::I(val))
 	}
 	let wc = if let Some(w) = vm.ws.iter().find(|&w| w.op == op)
-		{ w.code.clone() } else { return () };
+		{ w.code.clone() } else { return };
 	vmexec(vm, &wc[..])
 }
 
-static VMPRELUDE : &'static str = "[ 1 1 1 $ ] [dup] : \
-[ 1 0 $ ] [pop] : \
-[ 1 2 2 2 $ ] [swap] : \
-[ 1 3 2 3 3 $ ] [rsh3] : \
-[ 2 1 1 $ ] [over] : \
+static VMPRELUDE : &'static str = "[ 0 $ ] [popx] : \
+[ 1 popx ] [pop] : \
+[ 1 1 $ ] [dupx] : \
+[ 1 dupx ] [dup] : \
+[ 2 dupx ] [over] : \
+[ 1 2 4 2 $ ] [swap] : \
+[ 1 3 2 6 3 $ ] [rsh3] : \
+[ 2 1 3 6 3 $ ] [lsh3] : \
 [ ? . ] [if] : \
 [ [] rsh3 if ] [iff] : \
 [ -1 * ] [neg] : \
-[ prstr 10 prchr ] [prln] :";
+[ print 10 prchr ] [prln] :";
+//[ [prln prstack] depth dup prln iff ] [prstack] :";
 
 pub fn vmstart(code : &str) -> (){
 	let vm = &mut Vmem { st : Vec::new(), ws : Vec::new() };
@@ -176,12 +198,14 @@ fn vmexec(vm : &mut Vmem, code : &str) -> (){
 			"%/" => divmod(vm),
 			"$" => sform(vm),
 			"?" => pick(vm),
+			"@" => vm.st.push(Obj::S(String::from(code))),
 			"getch" => getchr(vm),
 			"print" => printobj(vm),
 			"prchr" => printchr(vm),
 			"depth" => pushdepth(vm),
 			"." => execstr(vm),
 			":" => defword(vm),
+			"ret" => return,
 			"" => (),
 			_ =>
 				if op.starts_with("["){
